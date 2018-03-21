baeb4a1d1c20317a1a8ca61a5d4df4e042c6d5de
Vartok Soma <vartok.soma@gmail.com>
2018-03-21 11:27:13 +0100

    popup if win

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 public class Klondike extends Application {
 
@@ -11,18 +12,17 @@ public class Klondike extends Application {
     private static final double WINDOW_HEIGHT = 900;
 
     public static void main(String[] args) {
-        launch(args);
+            launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        Card.loadCardImages();
-        Game game = new Game();
-        game.setTableBackground(new Image("/table/green.png"));
-
-        primaryStage.setTitle("Klondike Solitaire");
-        primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
-        primaryStage.show();
+            Card.loadCardImages();
+            Game game = new Game();
+            game.setTableBackground(new Image("/table/green.png"));
+            primaryStage.setTitle("Klondike Solitaire");
+            primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
+            primaryStage.show();
     }
 
 }
